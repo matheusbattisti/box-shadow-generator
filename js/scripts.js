b92ec8310c945c9c445379254aae7857b551a3f8
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-09-04 20:31:29 -0300

    continuando box shadow gen

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,8 @@ class BoxShadowGenerator {
     blurRef,
     spread,
     spreadRef,
+    color,
+    colorRef,
     previewBox,
     rule,
     webkitRule,
@@ -21,6 +23,8 @@ class BoxShadowGenerator {
     this.blurRef = blurRef;
     this.spread = spread;
     this.spreadRef = spreadRef;
+    this.color = color;
+    this.colorRef = colorRef;
     this.previewBox = previewBox;
     this.rule = rule;
     this.webkitRule = webkitRule;
@@ -32,14 +36,19 @@ class BoxShadowGenerator {
     this.verticalRef.value = this.vertical.value;
     this.blurRef.value = this.blur.value;
     this.spreadRef.value = this.spread.value;
+    this.colorRef.value = this.color.value;
 
     this.applyRule();
     this.showRule();
   }
 
   applyRule() {
-    this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000000`;
-    this.currentRule = this.previewBox.style.boxShadow;
+    const rgbValue = this.hexToRgb(this.colorRef.value);
+
+    const shadowRule = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px rgba(${rgbValue}, 1)`;
+
+    this.previewBox.style.boxShadow = shadowRule;
+    this.currentRule = shadowRule;
   }
 
   updateValue(type, value) {
@@ -56,6 +65,9 @@ class BoxShadowGenerator {
       case "blur":
         this.blurRef.value = value;
         break;
+      case "color":
+        this.colorRef.value = value;
+        break;
     }
 
     this.applyRule();
@@ -68,6 +80,12 @@ class BoxShadowGenerator {
     this.mozRule.innerText = this.currentRule;
     console.log(document.querySelector("#rule"));
     console.log(document.querySelector("#rule").innerText);
+  }
+
+  hexToRgb(hex) {
+    return `${("0x" + hex[1] + hex[2]) | 0}, ${("0x" + hex[3] + hex[4]) | 0}, ${
+      ("0x" + hex[5] + hex[6]) | 0
+    }`;
   }
 }
 
@@ -82,6 +100,9 @@ const spread = document.querySelector("#spread");
 const spreadRef = document.querySelector("#spread-value");
 const previewBox = document.querySelector("#box");
 
+const color = document.querySelector("#color");
+const colorRef = document.querySelector("#color-value");
+
 const rule = document.querySelector("#rule span");
 const webkitRule = document.querySelector("#webkit-rule span");
 const mozRule = document.querySelector("#moz-rule span");
@@ -95,6 +116,8 @@ const boxShadow = new BoxShadowGenerator(
   blurRef,
   spread,
   spreadRef,
+  color,
+  colorRef,
   previewBox,
   rule,
   webkitRule,
@@ -126,4 +149,12 @@ spread.addEventListener("input", (e) => {
   const value = e.target.value;
 
   boxShadow.updateValue("spread", value);
+});
+
+color.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  console.log(value);
+
+  boxShadow.updateValue("color", value);
 });
